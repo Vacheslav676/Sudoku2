57407cf5634555c176b4cacd8415a2fdd3fa7726
Vaceslav676 <vaceslavzubenko676@gmail.com>
2023-06-17 20:05:31 +0300

    добавил массив

--- app/src/main/java/com/example/mysudoku2/MainActivity.java
+++ app/src/main/java/com/example/mysudoku2/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.mysudoku2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity  {
+
+    public static void main(String[] args) throws IOException {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("MyLog", "Салам пополам");
+        Log.d("MyLog2", "Салам пополам2");
+        Log.i("MyLog", "Салам пополам");
+
+        int[][] sudokuTable = new int[10][10];
+
+        int testInt = 10;
+
     }
+
 }
